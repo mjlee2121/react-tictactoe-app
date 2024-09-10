5c0e2b4760cafcdbd788d57f5aebcba2d6970cbb
minji <lee.minji9521@gmail.com>
2024-09-10 18:14:11 -0400

    added super

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,18 +3,11 @@ import './Square.css'
 
 export default class Square extends React.Component {
     
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        }
-    }
-
     render(){
         return(
             <div>
-                <button className='square' onClick={()=>{this.setState({value: 'X'})}} >
-                    {this.state.value}
+                <button className='square' onClick={()=>{{this.setState({value:'X'})}}}>
+                    {this.props.value}
                 </button>
                
             </div>
